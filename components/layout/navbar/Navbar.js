f6628e84a37e3603ad69ab63ebe766df104f25cd
Micha <michael.rigas@t-online.de>
2024-01-30 15:02:28 +0100

    Alles neu

--- components/layout/navbar/Navbar.js
+++ components/layout/navbar/Navbar.js
@@ -1,12 +1,1 @@
-// navbar.js
-export default function Navbar() {
-  // Hier wird der JavaScript-Code für deine Navbar-Komponente definiert
-  const navbar = document.createElement("div");
-  navbar.className = "navbar";
-  navbar.innerHTML = `
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <button>Button</button>
-    `;
-  return navbar;
-}
+
